8d633dd55efa84c4fcaffe5d6d660f88d0a34a1d
PravAl2028 <anachipravalgupta@gmail.com>
2026-04-30 00:24:00 +0530

    Polish 1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kitchenmanager.app',
+  appName: 'KM-Kitchen Manager',
+  webDir: 'dist'
+};
+
+export default config;
